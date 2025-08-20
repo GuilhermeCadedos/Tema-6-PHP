e52ba8e9746f468ac9b4df3967affef48fcdfae6
GuilhermeCadedos <126833715+GuilhermeCadedos@users.noreply.github.com>
2025-08-19 21:12:01 -0300

    Add files via upload

---
+++ Tema6-PHP/Desafio3/main.php
@@ -0,0 +1,21 @@
+<!-- main.php -->
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Desafio com Include</title>
+</head>
+
+<body>
+    <?php include 'functions.php'; ?>
+    <h2>Desafio com Include</h2>
+    <?php
+    //Apenas atribuir valores às variáveis
+    $num1 = 4;
+    $num2 = 7;
+    echo "<p>A soma de $num1 e $num2 é: " . calcularSoma($num1, $num2) . "</p>";
+    ?>
+</body>
+
+</html>
